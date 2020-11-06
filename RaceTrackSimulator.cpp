1b8043555b4711966b84a71fdbab6d8cb966fd09
Daelin Campleman <daelinc@gmail.com>
2020-11-06 13:25:26 +0000

    Added simulate() function
    Added code for simulate() function including prints of results and saving of results. Difficulty of track is calculated as the length of the track multiplied by the number of turns divided by the length of the longest straight. It's random but its a number, if anyone has a better suggestion feel free to change it.

--- RaceTrackSimulator.cpp
+++ RaceTrackSimulator.cpp
@@ -1,4 +1,7 @@
 #include "RaceTrackSimulator.h"
+#include <iostream>
+
+using namespace std;
 
 RaceTrackSimulator::RaceTrackSimulator(){
 
@@ -9,7 +12,18 @@ RaceTrackSimulator::~RaceTrackSimulator(){
 }
 
 void RaceTrackSimulator::simulate(){
+	double length = 1000;
+	int turns = 12;
+	double straight = 50.2;
+	double difficulty = length*turns/straight;
 	
+	cout << "\033[4;37;Track simulation results:\033[0m" << endl;
+	cout << "\033[1;38;42mTotal Length: \033[0m\033[38;42m" << length << "\033[0m" << endl;
+	cout << "\033[1;38;42mTurns: \033[0m\033[38;42m" << turns << "\033[0m" << endl;
+	cout << "\033[1;38;42mLongest straight: \033[0m\033[38;42m" << straight << "\033[0m" << endl;
+
+	cout << "\033[4;37;42mDifficulty: \033[0m\033[1;4;37;42m" << difficulty << "\033[0m" << endl;
+	setResult(difficulty);
 }
 
 void RaceTrackSimulator::setSubject(VirtualRaceTrack* subject){
