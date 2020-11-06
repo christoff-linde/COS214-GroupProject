13ea53f41c5be0c643a5960769b6420d105d58ad
Daelin Campleman <daelinc@gmail.com>
2020-11-06 13:09:18 +0000

    update simulate() function
    Added the printing of results from a simulation in the simulate() function as well as setting the result from the simulation. The score is currently calculated as the average of the four parts of the car but this will change.

--- CarSimulator.cpp
+++ CarSimulator.cpp
@@ -1,4 +1,7 @@
 #include "CarSimulator.h"
+#include <iostream>
+
+using namespace std;
 
 CarSimulator::CarSimulator(){
 
@@ -9,7 +12,16 @@ CarSimulator::~CarSimulator(){
 }
 
 void CarSimulator::simulate(){
-	
+	int total = subject->getEngineLevel()+subject->getAerodynamicsLevel()+subject->getChassisLevel()+subject->getElectronicsLevel();
+	double score = total/4;
+	cout << "\033[4;37;42mCar simulation results:\033[0m" << endl;
+	cout << "\033[1;38;42mEngine: \033[0m\033[38;42m" << subject->getEngineLevel() << "\033[0m" << endl;
+	cout << "\033[1;38;42mAerodynamics: \033[0m\033[38;42m" << subject->getAerodynamicsLevel() << "\033[0m" << endl;
+	cout << "\033[1;38;42mChassis: \033[0m\033[38;42m" << subject->getChassisLevel() << "\033[0m" << endl;
+	cout << "\033[1;38;42mElectronics: \033[0m\033[38;42m" << subject->getElectronicsLevel() << "\033[0m" << endl << endl;
+
+	cout << "\033[4;37;42mTotal: \033[0m\033[1;4;37;42m" << score << "\033[0m" << endl;
+	setResult(score);
 }
 
 void CarSimulator::setSubject(VirtualRaceCar* subject){
