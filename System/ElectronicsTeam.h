f65cf92cda948c71784cef64d27bfa3e6cb86c83
Kyle Proctor-Parker <70330095+Kyle-Proctor-Parker@users.noreply.github.com>
2020-11-08 12:26:43 +0200

    Added doxygen to engineering

--- System/ElectronicsTeam.h
+++ System/ElectronicsTeam.h
@@ -1,3 +1,14 @@
+/**
+ * @file ElectronicsTeam.h
+ * @author Werner Graaff
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-08
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #ifndef ElectronicsTeam_H
 #define ElectronicsTeam_H
 #include <iostream>
@@ -11,8 +22,23 @@ using namespace std;
     class ElectronicsTeam : public EngineeringTeam {
         
         public:
+            /**
+             * @brief Construct a new Electronics Team object
+             * 
+             */
             ElectronicsTeam();
+
+            /**
+             * @brief Destroy the Electronics Team object
+             * 
+             */
             ~ElectronicsTeam();
+
+            /**
+             * @brief Create a Car Part object
+             * 
+             * @return CarPart* 
+             */
             CarPart* createCarPart();
 
         private:
--- System/EngineeringTeam.h
+++ System/EngineeringTeam.h
@@ -1,3 +1,13 @@
+/**
+ * @file EngineeringTeam.h
+ * @author Werner Graaff
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-08
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef EngineeringTeam_H
 #define EngineeringTeam_H
 #include <iostream>
@@ -18,13 +28,52 @@ using namespace std;
     class EngineeringTeam {
         
         public:
+            /**
+             * @brief Construct a new Engineering Team object
+             * 
+             */
             EngineeringTeam();
+
+            /**
+             * @brief Construct a new Engineering Team object
+             * 
+             * @param strategy 
+             */
             EngineeringTeam(RacingStrategy* strategy);
 
+            /**
+             * @brief Destroy the Engineering Team object
+             * 
+             */
             virtual ~EngineeringTeam();
+
+            /**
+             * @brief Create a Car Part object
+             * 
+             * @return CarPart* 
+             */
             virtual CarPart* createCarPart() = 0;
+
+            /**
+             * @brief Get the Strategy object
+             * 
+             * @return RacingStrategy* 
+             */
             RacingStrategy* getStrategy();
+
+            /**
+             * @brief Get the Parts List object
+             * 
+             * @return list <CarPart*> 
+             */
             list <CarPart*> getPartsList();
+
+            /**
+             * @brief 
+             * 
+             * @param partsList 
+             * @return RaceCar* 
+             */
             RaceCar* assembleCar(list <CarPart*> partsList);
             
         private:
