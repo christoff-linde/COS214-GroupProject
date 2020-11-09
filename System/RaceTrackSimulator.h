6345a1cd38a06509ca74c983edbd0a54985225e6
Daelin Campleman <46959640+Daelinc@users.noreply.github.com>
2020-11-09 16:40:07 +0000

    Updated for new RaceTrack & added comments

--- System/RaceTrackSimulator.h
+++ System/RaceTrackSimulator.h
@@ -1,3 +1,13 @@
+/**
+ * @file RaceTrackSimulator.h
+ * @author Daelin Campleman
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-09
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef RACETRACKSIMULATOR_H
 #define RACETRACKSIMULATOR_H
 
@@ -8,13 +18,33 @@ class RaceTrackSimulator : public Simulator
 {
 
 	public:
-
+		/**
+         * @brief Construct Race Track Simulator
+         */
 		RaceTrackSimulator();
+		/**
+         * @brief Destroy Race Track Simulator
+         */
 		virtual ~RaceTrackSimulator();
+		/**
+         * @brief Set the subject object
+         * 
+         * @param subject
+         */
 		void setSubject(VirtualRaceTrack* subject);
+		/**
+         * @brief Get the subject object
+         * 
+         * @return VirtualRaceTrack* 
+         */
 		VirtualRaceTrack* getSubject();
 
 	protected:
+		/**
+         * @brief Simulate the subject
+         * 
+         * @return void
+         */
 		void simulate();
 
 	private:
--- System/VirtualRaceTrack.h
+++ System/VirtualRaceTrack.h
@@ -1,3 +1,13 @@
+/**
+ * @file VirtualRaceTrack.h
+ * @author Daelin Campleman
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-09
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef VIRTUALRACETRACK_H
 #define VIRTUALRACETRACK_H
 
@@ -5,17 +15,46 @@ class VirtualRaceTrack
 {
 
 	public:
-
+		/**
+         * @brief Construct Virtual Race Track
+         */
 		VirtualRaceTrack();
+		/**
+         * @brief Destroy Virtual Race Track
+         */
 		virtual ~VirtualRaceTrack();
+		/**
+         * @brief Get the score
+         * 
+         * @return double
+         */
 		virtual double getScore();
+		/**
+         * @brief Get the total length
+         * 
+         * @return double
+         */
 		virtual double getTotalLength();
+		/**
+         * @brief Get the number of turns
+         * 
+         * @return double
+         */
 		virtual int getTurns();
+		/**
+         * @brief Get the length of the longest straight
+         * 
+         * @return double
+         */
 		virtual double getLongestStraight();
 
 
 	protected:
-
+		/**
+         * @brief Set the score
+         * 
+         * @param s
+         */
 		void setScore(double s);
 
 	private:
